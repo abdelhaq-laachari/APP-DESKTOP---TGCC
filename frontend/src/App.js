18a447677d90b3a3a2796d6e2063dd53451ee85c
Abdelhaq laachari <93976987+abdelhaq-laachari@users.noreply.github.com>
2023-03-30 12:23:56 +0000

    setup file

---
+++ frontend/src/App.js
@@ -0,0 +1,13 @@
+
+import Dashboard from './Page/Dashboard/Dashboard';
+import React from 'react';
+
+function App() {
+  return (
+    <div>
+      <Dashboard />
+    </div>
+  );
+}
+
+export default App;
